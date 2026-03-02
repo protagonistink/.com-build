301bb452cd633fbd05aeb40c019338fbfe1ff000
Patrick Kirkland <patrick@protagonist.ink>
2026-03-02 10:33:48 -0500

    Remove legacy Blog Post schema from Studio

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,5 @@
-import { blogPost } from './blogPost';
 import { post } from './post';
 import { category } from './category';
 import { caseStudy } from './caseStudy';
 
-export const schemaTypes = [blogPost, post, category, caseStudy];
+export const schemaTypes = [post, category, caseStudy];
